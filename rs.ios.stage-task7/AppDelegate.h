32a6830d9fac21cd577f1f9ecd5ad5ecf4270d78
zm1ca <zmicier.fied@yandex.ru>
2021-07-02 08:53:27 +0300

    Initial Commit

---
+++ rs.ios.stage-task7/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  rs.ios.stage-task7
+//
+//  Created by Źmicier Fiedčanka on 2.07.21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ rs.ios.stage-task7/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  rs.ios.stage-task7
+//
+//  Created by Źmicier Fiedčanka on 2.07.21.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
